95af356a2c0177135ac345d0c9275c3b99a54586
agakshita5 <akshita.ag2020@gmail.com>
2026-05-01 15:36:49 +0530

    feat: components position fixed

--- components/ImagePreview.tsx
+++ components/ImagePreview.tsx
@@ -6,27 +6,32 @@ export default function ImagePreview(){
             <div className="flex-1 flex flex-col bg-black overflow-hidden">
 
                 {/* MAIN AREA */}
-                <div className="flex-1 overflow-auto flex items-center justify-center p-4">
-
-                    {/* CANVAS (image area) */}
-                    <div className="max-w-5xl w-full flex items-center justify-center">
-                        <div className="bg-slate-800 w-full min-h-[600px] flex items-center justify-center rounded-xl shadow-2xl">
-                            
-                            <div className="relative w-full flex items-center justify-center p-6">
-                                {/* background blur */}
-                                <div className="absolute inset-0 flex items-center justify-center z-0">
-                                    <div className="w-[85%] h-[85%] bg-black/60 blur-2xl rounded-lg"></div>
+                <div className="flex-1 overflow-auto flex items-center justify-center p-4 pb-10">
+                    <div className="flex items-center justify-center w-full h-full">
+                        {/* CANVAS (image area) */}
+                        <div className="w-full max-w-5xl flex items-center justify-center">
+                            <div className="relative w-full">
+                                {/* Canvas with blur background */}
+                                <div className="w-full min-h-[600px] flex items-center justify-center rounded-xl shadow-2xl relative">
+                                    <div className="absolute inset-0 flex items-center justify-center z-0">
+                                        <div className="w-[85%] h-[85%] bg-black/60 blur-2xl rounded-lg"></div>
+                                    </div>
+                                    
+                                    {/* image */}
+                                    <img 
+                                        className="rounded-xl z-10 shadow-2xl shadow-[0_0_60px_10px_rgba(22,36,86,0.5)] max-h-[550px] max-w-full object-contain hover:scale-[1.02] transition duration-300" 
+                                        src="output_image/nature-image.jpg" 
+                                        alt="Generated preview"
+                                    />
                                 </div>
-                                {/* image */}
-                                <img 
-                                    className="rounded-xl z-10 shadow-2xl shadow-[0_20px_60px_rgba(0,0,0,0.6)] max-h-[600px] max-w-full object-contain hover:scale-[1.02] transition duration-300" 
-                                    src="output_image/nature-image.jpg" 
-                                    alt="nature" />
                             </div>
                         </div>
                     </div>
                 </div>
-                <Download />
+
+                <div className="absolute top-4 right-4 z-20">
+                    <Download />
+                </div>
             </div>
         </>
     );
